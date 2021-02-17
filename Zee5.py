15054fdf6a2bb30f6be8cb5c365c3fe91b523351
Akash R Chandran <78685510+akashrchandran@users.noreply.github.com>
2021-02-17 14:54:04 +0530

    update Zee5.py

--- Zee5.py
+++ Zee5.py
@@ -39,7 +39,7 @@ def Voot_helper(chat_id, combo):
         text = f'<b>Bad Combo ❌</b>\n<b>Combo: </b><code>{combo}</code>\n<b>Status: Error\nCode: {code}\nMessage: {msg}\nSite: Zee5</b>'
         Editmessage(chat_id, text, status)
         return
-    # acess = response['data']['authToken']['accessToken']
+     acess = response['data']['authToken']['accessToken']
     # head2 = {
     #     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
     #     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
