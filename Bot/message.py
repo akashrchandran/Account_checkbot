703ce4d4b8f82144fd16e7bf1abd8e5cd6125c4b
Akash R Chandran <78685510+akashrchandran@users.noreply.github.com>
2021-05-10 15:54:26 +0530

    Fixed Error

--- Bot/message.py
+++ Bot/message.py
@@ -8,7 +8,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-bot_token = os.environ.get('BOT_TOKEN')
+bot_token = os.environ.get('TG_BOT_TOKEN')
 bot = Bot(bot_token)
 
 def Sendmessage(chat_id, text,reply_markup=None):
