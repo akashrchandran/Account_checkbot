787db011be01ee16a76159da877d94be772fd566
Akash R Chandran <78685510+akashrchandran@users.noreply.github.com>
2021-05-25 01:19:10 +0530

    Update zee5.py

--- Bot/Checks/zee5.py
+++ Bot/Checks/zee5.py
@@ -51,9 +51,10 @@ def zee_helper(chat_id, combo):
     timedioint = result1[0]["subscription_end"].split('T')[0]
     sub2split = timedioint.split('-')
     trial = date(int(sub2split[0]), int(sub2split[1]), int(sub2split[2])) - date.today()
-    Pack_name = result1[0]['subscription_plan']['title'] + ' ' + str(result1[0]['subscription_plan']['price'])
+    Pack_name = result1[0]['subscription_plan']['title']
+    pack_price = str(result1[0]['subscription_plan']['price'])
     Pack_recur = result1[0]['recurring_enabled']
     Pack_pyed = result1[0]['payment_provider']
-    pro_message = f'<b>🌟 Hit Combo 💫</b>\n<b>Site: Zee5</b>\n<b>Combo: </b><code>{combo}</code>\n<b>Status: Premium\nPlan: {Pack_name}\nDays Left: {trial.days} Days\nPayment: {Pack_pyed}\nRecurring: {Pack_recur}</b>'
+    pro_message = f'<b>🌟 Hit Combo 💫</b>\n<b>Site: Zee5</b>\n<b>Combo: </b><code>{combo}</code>\n<b>Plan: {Pack_name}\nPrice: {Pack_name} INR\nDays Left: {trial.days} Days\nPayment: {Pack_pyed}\nRecurring: {Pack_recur}</b>'
     # print(pro_message)
     Editmessage(chat_id, pro_message, status)
